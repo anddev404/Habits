77c4fdd9819cc89b20795d61e88905af58e95f1e
anddev404 <marcinek07@gmail.com>
2024-07-02 19:20:30 +0200

    Create HabitsViewItemState.kt

---
+++ app/src/main/java/com/anddev404/main/components/habits_view/states/HabitsViewItemState.kt
@@ -0,0 +1,13 @@
+package com.anddev404.main.components.habits_view.states
+
+import androidx.compose.ui.graphics.Color
+import com.anddev404.ui.theme.InactiveDarkColor
+
+sealed class HabitsViewItemState() {
+    data class YesOrNo(val id: Int, var isChecked: Boolean, val color: Color = InactiveDarkColor) :
+        HabitsViewItemState()
+
+    data class Value(
+        val id: Int, var unit: String, var value: Int, var color: Color = InactiveDarkColor
+    ) : HabitsViewItemState()
+}
