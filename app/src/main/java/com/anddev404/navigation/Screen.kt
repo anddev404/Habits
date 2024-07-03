3583073309f0ca93904914aabfc57d60c7802b79
anddev404 <marcinek07@gmail.com>
2024-07-03 23:17:16 +0200

    Update Screen.kt

--- app/src/main/java/com/anddev404/navigation/Screen.kt
+++ app/src/main/java/com/anddev404/navigation/Screen.kt
@@ -3,4 +3,6 @@ package com.anddev404.navigation
 sealed class Screen(val route: String) {
     data object HabitsScreen : Screen("habit_screen")
     data object CreateHabitScreen : Screen("create_screen")
+    data object StatsScreen : Screen("stats_screen")
+
 }
