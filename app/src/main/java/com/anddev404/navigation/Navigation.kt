810180da58c0535cb0229557fe02b3449a62fa9a
anddev404 <marcinek07@gmail.com>
2024-07-03 23:09:15 +0200

    Create Navigation.kt

---
+++ app/src/main/java/com/anddev404/navigation/Navigation.kt
@@ -0,0 +1,26 @@
+package com.anddev404.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.anddev404.create_habit.presentation.CreateHabitScreen
+import com.anddev404.habits.components.HabitsScreen
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.HabitsScreen.route
+    ) {
+        composable(route = Screen.HabitsScreen.route) {
+            HabitsScreen(navController = navController)
+        }
+        composable(route = Screen.CreateHabitScreen.route) {
+            CreateHabitScreen()
+        }
+
+    }
+}
