e9dc22217681cd1b68b2464695207aae67a8711d
anddev404 <marcinek07@gmail.com>
2024-07-03 23:07:35 +0200

    Create empty HabitScreen

---
+++ app/src/main/java/com/anddev404/habits/presentation/HabitsScreen.kt
@@ -0,0 +1,21 @@
+package com.anddev404.habits.components
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.anddev404.habits.components.main_top_bar.MainTopBar
+
+@Composable
+fun HabitsScreen(navController: NavController) {
+
+    Scaffold(modifier = Modifier.fillMaxSize(),
+        topBar = {
+            MainTopBar()
+        }) { innerPadding ->
+        Text(modifier = Modifier.padding(innerPadding), text = "Habits ... ")
+    }
+}
