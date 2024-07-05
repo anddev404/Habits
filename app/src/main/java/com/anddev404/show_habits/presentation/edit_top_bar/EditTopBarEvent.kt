174d29c2fd764b5733e9679dd14739f8c164e73e
anddev404 <marcinek07@gmail.com>
2024-07-05 10:47:53 +0200

    Create EditTopBarEvent.kt

---
+++ app/src/main/java/com/anddev404/show_habits/presentation/edit_top_bar/EditTopBarEvent.kt
@@ -0,0 +1,9 @@
+package com.anddev404.show_habits.presentation.edit_top_bar
+
+sealed class EditTopBarEvent {
+    data object OnBackClick : EditTopBarEvent()
+    data class OnEditClick(val id: Int) : EditTopBarEvent()
+    data class OnChangeColorClick(val id: Int, val color: Int) : EditTopBarEvent()
+    data class OnArchiveClick(val ids: List<Int>) : EditTopBarEvent()
+    data class OnDeleteClick(val ids: List<Int>) : EditTopBarEvent()
+}
