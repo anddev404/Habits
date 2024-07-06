5b307cd20650ee87c6cb14a663b121f08e736bcd
anddev404 <marcinek07@gmail.com>
2024-07-06 09:28:47 +0200

    Update Screen.kt

--- app/src/main/java/com/anddev404/navigation/Screen.kt
+++ app/src/main/java/com/anddev404/navigation/Screen.kt
@@ -5,4 +5,7 @@ sealed class Screen(val route: String) {
     data object CreateHabitScreen : Screen("create_screen")
     data object StatsScreen : Screen("stats_screen")
 
+    data object CreateHabitYesOrNoContent : Screen("create_habit_yes_or_no_content")
+    data object CreateHabitMeasurableContent : Screen("create_habit_measurable_content")
+
 }
