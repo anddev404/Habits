fc03bb9e8e1c0a7078f208df5f6020adea0ef87a
anddev404 <marcinek07@gmail.com>
2024-07-09 17:21:52 +0200

    Update YesOrNoFrequencyState.kt

--- app/src/main/java/com/anddev404/create_habit/presentation/yes_or_no_content/YesOrNoFrequencyState.kt
+++ app/src/main/java/com/anddev404/create_habit/presentation/yes_or_no_content/YesOrNoFrequencyState.kt
@@ -1,8 +1,12 @@
 package com.anddev404.create_habit.presentation.yes_or_no_content
 
-sealed class YesOrNoFrequencyState() {
-    data object DailyYesOrNoFrequency : YesOrNoFrequencyState()
-    data class IntervalYesOrNoFrequency(val frequency: Int) : YesOrNoFrequencyState()
-    data class WeeklyYesOrNoFrequency(val frequency: Int) : YesOrNoFrequencyState()
-    data class MonthlyYesOrNoFrequency(val frequency: Int) : YesOrNoFrequencyState()
+enum class YesOrNoFrequencyState(
+    var frequency: Int = -1,
+    var startText: String = "",
+    var endText: String = ""
+) {
+    DAILY(-1),
+    INTERVAL(3),
+    WEEKLY(3),
+    MONTHLY(10);
 }
