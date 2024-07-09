77936256cbaf06a5d7b5dfddf55e22d0de6f35e4
anddev404 <marcinek07@gmail.com>
2024-07-09 17:27:48 +0200

    Update CreateHabitMeasurableContent.kt

--- app/src/main/java/com/anddev404/create_habit/presentation/measurable_content/CreateHabitMeasurableContent.kt
+++ app/src/main/java/com/anddev404/create_habit/presentation/measurable_content/CreateHabitMeasurableContent.kt
@@ -1,8 +1,9 @@
 package com.anddev404.create_habit.presentation.measurable_content
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun CreateHabitMeasurableContent() {
-
+    Text(text = "TODO")
 }
