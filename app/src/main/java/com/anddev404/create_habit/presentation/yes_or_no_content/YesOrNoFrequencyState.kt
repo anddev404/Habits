f1266badb61722edc38e78ca12e009ed35c70308
anddev404 <marcinek07@gmail.com>
2024-07-09 19:28:57 +0200

    Update YesOrNoFrequencyState.kt

--- app/src/main/java/com/anddev404/create_habit/presentation/yes_or_no_content/YesOrNoFrequencyState.kt
+++ app/src/main/java/com/anddev404/create_habit/presentation/yes_or_no_content/YesOrNoFrequencyState.kt
@@ -1,5 +1,8 @@
 package com.anddev404.create_habit.presentation.yes_or_no_content
 
+import android.content.Context
+import com.anddev404.R
+
 enum class YesOrNoFrequencyState(
     var frequency: Int = -1,
     var startText: String = "",
@@ -10,8 +13,19 @@ enum class YesOrNoFrequencyState(
     WEEKLY(3),
     MONTHLY(10);
 
-    override fun toString(): String {
+    fun toString(context: Context): String {
         val frequency: String = if (frequency > 0) frequency.toString() else ""
+
+        when (this) {
+            DAILY -> startText = context.getString(R.string.daily)
+            INTERVAL -> {
+                startText = context.getString(R.string.interval_start_text)
+                endText = context.getString(R.string.interval_end_text)
+            }
+
+            WEEKLY -> endText = context.getString(R.string.weekly)
+            MONTHLY -> endText = context.getString(R.string.monthly)
+        }
         return "${startText} $frequency ${endText}".trim()
     }
 }
