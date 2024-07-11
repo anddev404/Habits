63aa36cd93f4b63dc9a43b671f9357be03172b1e
anddev404 <marcinek07@gmail.com>
2024-07-11 19:52:54 +0200

    Update HabitDao.kt

--- app/src/main/java/com/anddev404/core/data/local/HabitDao.kt
+++ app/src/main/java/com/anddev404/core/data/local/HabitDao.kt
@@ -1,10 +1,13 @@
 package com.anddev404.core.data.local
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.anddev404.core.data.local.entity.HabitEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface HabitDao {
@@ -12,6 +15,12 @@ interface HabitDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(habit: HabitEntity)
 
+    @Update
+    suspend fun update(habit: HabitEntity)
+
+    @Delete
+    suspend fun delete(habit: HabitEntity)
+
     @Query("SELECT * FROM HabitEntity")
-    suspend fun getAll(): List<HabitEntity>
+    suspend fun getAll(): Flow<List<HabitEntity>>
 }
