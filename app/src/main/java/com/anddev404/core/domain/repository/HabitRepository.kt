f9d4edea519c0cc5315231b0d8a655f3127e0e52
anddev404 <marcinek07@gmail.com>
2024-07-11 19:53:17 +0200

    Create HabitRepository.kt

---
+++ app/src/main/java/com/anddev404/core/domain/repository/HabitRepository.kt
@@ -0,0 +1,17 @@
+package com.anddev404.core.domain.repository
+
+import androidx.room.Query
+import com.anddev404.core.data.local.entity.HabitEntity
+import kotlinx.coroutines.flow.Flow
+
+interface HabitRepository {
+
+    @Query("SELECT * FROM HabitEntity")
+    suspend fun getAllHabits(): Flow<List<HabitEntity>>
+
+    suspend fun insertHabit(habit: HabitEntity)
+
+    suspend fun updateHabit(habit: HabitEntity)
+
+    suspend fun deleteHabit(habit: HabitEntity)
+}
