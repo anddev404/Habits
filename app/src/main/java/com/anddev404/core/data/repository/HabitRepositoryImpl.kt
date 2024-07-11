0201da378450eb8b34da89191bdcd905534024df
anddev404 <marcinek07@gmail.com>
2024-07-11 19:54:22 +0200

    Create HabitRepositoryImpl.kt

---
+++ app/src/main/java/com/anddev404/core/data/repository/HabitRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package com.anddev404.core.data.repository
+
+import com.anddev404.core.data.local.HabitDatabase
+import com.anddev404.core.data.local.entity.HabitEntity
+import com.anddev404.core.domain.repository.HabitRepository
+import kotlinx.coroutines.flow.Flow
+
+class HabitRepositoryImpl(private val database: HabitDatabase) : HabitRepository {
+
+    override suspend fun getAllHabits(): Flow<List<HabitEntity>> {
+        return database.habitDao().getAll()
+    }
+
+    override suspend fun insertHabit(habit: HabitEntity) {
+        database.habitDao().insert(habit)
+    }
+
+    override suspend fun updateHabit(habit: HabitEntity) {
+        database.habitDao().update(habit)
+    }
+
+    override suspend fun deleteHabit(habit: HabitEntity) {
+        database.habitDao().delete(habit)
+    }
+}
