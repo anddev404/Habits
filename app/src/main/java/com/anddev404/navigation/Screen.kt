32271794e00beb2fcb939fed39984f0483927a8e
anddev404 <marcinek07@gmail.com>
2024-07-11 11:22:06 +0200

    Update Screen.kt

--- app/src/main/java/com/anddev404/navigation/Screen.kt
+++ app/src/main/java/com/anddev404/navigation/Screen.kt
@@ -1,10 +1,13 @@
 package com.anddev404.navigation
 
+import kotlinx.serialization.Serializable
+
 sealed class Screen(val route: String) {
     data object HabitsScreen : Screen("habit_screen")
-    data object CreateHabitScreen : Screen("create_screen")
-    data object StatsScreen : Screen("stats_screen")
 
+    @Serializable
+    data class CreateHabitScreen(val contentId: Int)
+    data object StatsScreen : Screen("stats_screen")
     data object CreateHabitYesOrNoContent : Screen("create_habit_yes_or_no_content")
     data object CreateHabitMeasurableContent : Screen("create_habit_measurable_content")
 
