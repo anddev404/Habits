bb52f04a4b7ee8ff2aa6ce555835a3883081419e
anddev404 <marcinek07@gmail.com>
2024-07-11 11:22:56 +0200

    Update MainTopBarDialogEvent.kt

--- app/src/main/java/com/anddev404/show_habits/presentation/main_top_bar/dialogs/MainTopBarDialogEvent.kt
+++ app/src/main/java/com/anddev404/show_habits/presentation/main_top_bar/dialogs/MainTopBarDialogEvent.kt
@@ -1,6 +1,16 @@
 package com.anddev404.show_habits.presentation.main_top_bar.dialogs
 
-sealed class MainTopBarDialogEvent {
-    data object OnAddHabitYesOrNoClick : MainTopBarDialogEvent()
-    data object OnAddHabitMeasurableClick : MainTopBarDialogEvent()
+
+enum class MainTopBarDialogEvent(val contentId: Int) {
+    OnAddHabitYesOrNoClick(1),
+    OnAddHabitMeasurableClick(2);
+
+    companion object {
+        fun getByContentId(
+            contentId: Int,
+            default: MainTopBarDialogEvent = OnAddHabitYesOrNoClick
+        ): MainTopBarDialogEvent {
+            return MainTopBarDialogEvent.values().find { it.contentId == contentId } ?: default
+        }
+    }
 }
