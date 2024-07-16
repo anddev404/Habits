e855919dae9deb3c238c5d202b8d7c6121a968cb
anddev404 <marcinek07@gmail.com>
2024-07-16 15:55:34 +0200

    Update CreateHabitScreen and viewmodel

--- app/src/main/java/com/anddev404/create_habit/presentation/CreateHabitScreen.kt
+++ app/src/main/java/com/anddev404/create_habit/presentation/CreateHabitScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -22,17 +23,21 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color.Companion.Green
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.anddev404.R
 import com.anddev404.core.presentation.components.ColorPaletteDialog
+import com.anddev404.core.presentation.components.ErrorHandlingOutlinedTextField
+import com.anddev404.core.presentation.components.LoadingScreen
 import com.anddev404.create_habit.presentation.create_habit_top_bar.CreateHabitTopBar
 import com.anddev404.navigation.CreateHabitNavigation
 import com.anddev404.navigation.Screen
@@ -43,11 +48,30 @@ import com.anddev404.ui.theme.LocalSpacing
 @Composable
 fun CreateHabitScreen(
     state: MainTopBarDialogEvent = MainTopBarDialogEvent.OnAddHabitYesOrNoClick,
+    id: Int = 0,
+    viewModel: CreateHabitViewModel = hiltViewModel(),
     onBackClick: () -> Unit = {}
 ) {
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        topBar = { CreateHabitTopBar(onBackClick = { onBackClick() }) }) { innerPadding ->
+    if (id > 0) viewModel.getHabitById(id) else viewModel.createNewHabit()
+
+    Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+        CreateHabitTopBar(onBackClick = { onBackClick() }) {
+            viewModel.insertHabit() {
+                onBackClick()
+            }
+        }
+    }) { innerPadding ->
+
+        val viewState by viewModel.processState.collectAsState()
+        when (viewState) {
+            CreateHabitState.HabitLoading -> {
+                LoadingScreen(Modifier.padding(innerPadding))
+                return@Scaffold
+            }
+
+            else -> {}
+        }
+
         Column(
             modifier = Modifier
                 .padding(innerPadding)
@@ -60,30 +84,31 @@ fun CreateHabitScreen(
                         .fillMaxWidth()
                         .height(IntrinsicSize.Max)
                 ) {
-                    OutlinedTextField(
-                        modifier = Modifier.weight(1.0f),
-                        value = " ",
-                        onValueChange = {},
-                        label = { Text(text = stringResource(id = R.string.name)) },
-                        placeholder = { Text(text = stringResource(id = R.string.name_example)) }
+                    val habitState by viewModel.habitState.collectAsState()
+
+                    ErrorHandlingOutlinedTextField(
+                        Modifier.weight(1.0f),
+                        habitState.name,
+                        {
+                            viewModel.updateHabitState(
+                                habitState.copy(
+                                    name = it,
+                                    isValidName = true
+                                )
+                            )
+                        },
+                        stringResource(id = R.string.name),
+                        stringResource(id = R.string.name_example),
+                        isValid = habitState.isValidName,
+                        errorText = stringResource(id = R.string.empty_field)
                     )
+
                     Spacer(modifier = Modifier.width(LocalSpacing.current.spaceSmall))
                     Box(
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .width(64.dp)
+                        modifier = Modifier.size(64.dp)
                     ) {
-                        val showColorPalette by remember {
-                            mutableStateOf(false)
-                        }
-                        if (showColorPalette) ColorPaletteDialog() {
-                            if (it == 0) return@ColorPaletteDialog
-
-                        }
                         OutlinedTextField(
-                            modifier = Modifier
-                                .fillMaxHeight()
-                                .width(64.dp),
+                            modifier = Modifier.fillMaxSize(),
                             value = " ",
                             onValueChange = {
 
@@ -91,18 +116,31 @@ fun CreateHabitScreen(
                             label = { Text(text = stringResource(id = R.string.color)) },
                             readOnly = true
                         )
-                        Spacer(
-                            modifier = Modifier
-                                .clickable {
-
-                                }
-                                .fillMaxHeight()
-                                .fillMaxWidth()
-                                .padding(top = 24.dp, start = 16.dp, end = 16.dp, bottom = 16.dp)
-                                .clip(RoundedCornerShape(2.dp))
-                                .background(Green)
-                                .border(1.dp, InactiveLightColor)
+                        var showColorPalette by remember {
+                            mutableStateOf(false)
+                        }
+                        if (showColorPalette) ColorPaletteDialog(habitState.color) {
+                            showColorPalette = false
+                            if (it == 0) return@ColorPaletteDialog
+                            viewModel.updateHabitState(habitState.copy(color = it))
+                        }
+                        OutlinedTextField(
+                            modifier = Modifier.fillMaxSize(),
+                            value = " ",
+                            onValueChange = {},
+                            label = { Text(text = stringResource(id = R.string.color)) },
+                            readOnly = true
                         )
+                        Spacer(modifier = Modifier
+                            .clickable {
+                                showColorPalette = true
+                            }
+                            .fillMaxHeight()
+                            .fillMaxWidth()
+                            .padding(top = 24.dp, start = 16.dp, end = 16.dp, bottom = 16.dp)
+                            .clip(RoundedCornerShape(2.dp))
+                            .background(Color(habitState.color))
+                            .border(1.dp, InactiveLightColor))
                     }
                 }
 
@@ -112,35 +150,25 @@ fun CreateHabitScreen(
                 }
                 Spacer(modifier = Modifier.width(LocalSpacing.current.spaceSmall))
 
-                OutlinedTextField(
-                    modifier = Modifier.fillMaxWidth(),
+                OutlinedTextField(modifier = Modifier.fillMaxWidth(),
                     value = stringResource(id = R.string.turned_off),
                     enabled = false,
                     onValueChange = {},
                     label = { Text(text = stringResource(id = R.string.reminder)) },
                     trailingIcon = {
                         Icon(imageVector = Icons.Default.ArrowDropDown, "")
-                    }
-                )
+                    })
                 Spacer(modifier = Modifier.width(LocalSpacing.current.spaceSmall))
 
-                OutlinedTextField(
-                    modifier = Modifier.fillMaxWidth(),
+                OutlinedTextField(modifier = Modifier.fillMaxWidth(),
                     value = " ",
                     enabled = false,
                     onValueChange = {},
-                    label = { Text(text = stringResource(id = R.string.notes)) }
-                )
+                    label = { Text(text = stringResource(id = R.string.notes)) })
                 Spacer(modifier = Modifier.width(LocalSpacing.current.spaceSmall))
             }
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun CreateHabitScreenPreview() {
-    CreateHabitScreen()
 }
 
 
--- app/src/main/java/com/anddev404/create_habit/presentation/CreateHabitViewModel.kt
+++ app/src/main/java/com/anddev404/create_habit/presentation/CreateHabitViewModel.kt
@@ -2,8 +2,10 @@ package com.anddev404.create_habit.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.anddev404.core.data.local.entity.HabitEntity
+import com.anddev404.core.data.mapper.toHabit
+import com.anddev404.core.data.mapper.toHabitEntity
 import com.anddev404.core.domain.repository.HabitRepository
+import com.anddev404.show_habits.domain.model.Habit
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -18,21 +20,39 @@ class CreateHabitViewModel @Inject constructor(private val repository: HabitRepo
         MutableStateFlow(CreateHabitState.HabitLoading)
     val processState = _processState.asStateFlow()
 
-    private val _habitEntity = MutableStateFlow(HabitEntity())
-    val habitEntity = _habitEntity.asStateFlow()
+    private val _habitState = MutableStateFlow(Habit())
+    val habitState = _habitState.asStateFlow()
 
     fun getHabitById(id: Int) {
         viewModelScope.launch {
             repository.getHabitById(id)
-                .collect { habit ->
-                    habit?.let {
-                        _habitEntity.value = habit
+                .collect { habitEntity ->
+                    habitEntity?.let {
+                        _habitState.value = habitEntity.toHabit()
                         _processState.value = CreateHabitState.HabitEditing
                         return@collect
                     }
-                    _habitEntity.value = HabitEntity()
                     _processState.value = CreateHabitState.HabitDoesNotExist
                 }
+        }
+    }
+
+    fun createNewHabit() {
+        _processState.value = CreateHabitState.HabitCreating
+    }
+
+    fun updateHabitState(habit: Habit) {
+        _habitState.value = habit
+    }
+
+    fun insertHabit(success: () -> Unit = {}) {
+        viewModelScope.launch {
+            if (_habitState.value.name.text.isEmpty()) {
+                _habitState.value = _habitState.value.copy(isValidName = false)
+            } else {
+                repository.insertHabit(_habitState.value.toHabitEntity())
+                success()
+            }
         }
     }
 }
