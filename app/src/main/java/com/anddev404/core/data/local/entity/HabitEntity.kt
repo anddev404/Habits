d234ecd307f079262849280cb480eefd6d5959a1
anddev404 <marcinek07@gmail.com>
2024-07-17 07:53:03 +0200

    Update HabitEntity.kt

--- app/src/main/java/com/anddev404/core/data/local/entity/HabitEntity.kt
+++ app/src/main/java/com/anddev404/core/data/local/entity/HabitEntity.kt
@@ -5,17 +5,17 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class HabitEntity(
-    val name: String = "",
-    val notes: String = "",
-    val color: Int = 0,
-    val isArchived: Boolean = false,
-    val position: Int = 0,
-    val reminder: Long = 0,
-    val habitType: Int = 0,
-    val frequencyNumerator: Int = 0,
-    val frequencyDenominator: Int = 0,
-    val unit: String = "",
-    val target: Double = 0.0,
-    val targetType: Int = 0,
-    @PrimaryKey val id: Int? = null
+    val name: String,
+    val notes: String,
+    val color: Int,
+    val isArchived: Boolean,
+    val position: Int,
+    val reminder: Long,
+    val habitType: Int,
+    val frequencyNumerator: Int,
+    val frequencyDenominator: Int,
+    val unit: String,
+    val target: Double,
+    val targetType: Int,
+    @PrimaryKey(true) val id: Int = 0
 )
