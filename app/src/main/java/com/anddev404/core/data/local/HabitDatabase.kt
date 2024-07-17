7635f608639574207c683dee99112bff6989ee58
anddev404 <marcinek07@gmail.com>
2024-07-17 07:57:44 +0200

    Update HabitDatabase.kt

--- app/src/main/java/com/anddev404/core/data/local/HabitDatabase.kt
+++ app/src/main/java/com/anddev404/core/data/local/HabitDatabase.kt
@@ -3,9 +3,11 @@ package com.anddev404.core.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.anddev404.core.data.local.entity.HabitEntity
+import com.anddev404.core.data.local.entity.ItemEntity
 
 
-@Database(entities = [HabitEntity::class], version = 1)
+@Database(entities = [HabitEntity::class, ItemEntity::class], version = 1)
 abstract class HabitDatabase : RoomDatabase() {
     abstract fun habitDao(): HabitDao
+    abstract fun itemDao(): ItemDao
 }
