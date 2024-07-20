533f28a8b1e4d95af233bafbd6f58eefb795a080
anddev404 <marcinek07@gmail.com>
2024-07-20 16:20:16 +0200

    Update CreateHabitViewModel.kt

--- app/src/main/java/com/anddev404/create_habit/presentation/CreateHabitViewModel.kt
+++ app/src/main/java/com/anddev404/create_habit/presentation/CreateHabitViewModel.kt
@@ -4,17 +4,18 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.anddev404.core.data.mapper.toHabit
 import com.anddev404.core.data.mapper.toHabitEntity
-import com.anddev404.core.domain.repository.HabitRepository
+import com.anddev404.create_habit.domain.use_case.CreateHabitUseCases
 import com.anddev404.show_habits.domain.model.Habit
 import com.anddev404.show_habits.presentation.main_top_bar.dialogs.MainTopBarDialogEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class CreateHabitViewModel @Inject constructor(private val repository: HabitRepository) :
+class CreateHabitViewModel @Inject constructor(private val habitUseCases: CreateHabitUseCases) :
     ViewModel() {
 
     private val _processState: MutableStateFlow<CreateHabitState> =
@@ -25,13 +26,14 @@ class CreateHabitViewModel @Inject constructor(private val repository: HabitRepo
     val habitState = _habitState.asStateFlow()
 
     fun getHabitById(id: Int) {
-        viewModelScope.launch {
-            repository.getHabitById(id)?.let {
+        viewModelScope.launch(Dispatchers.IO) {
+
+            habitUseCases.getHabit(id).onSuccess {
                 _habitState.value = it.toHabit()
                 _processState.value = CreateHabitState.HabitEditing
-                return@launch
+            }.onFailure {
+                _processState.value = CreateHabitState.HabitDoesNotExist
             }
-            _processState.value = CreateHabitState.HabitDoesNotExist
         }
     }
 
@@ -49,7 +51,7 @@ class CreateHabitViewModel @Inject constructor(private val repository: HabitRepo
             if (_habitState.value.name.text.isEmpty()) {
                 _habitState.value = _habitState.value.copy(isValidName = false)
             } else {
-                repository.insertHabit(_habitState.value.toHabitEntity())
+                habitUseCases.insertHabit(_habitState.value.toHabitEntity())
                 success()
             }
         }
