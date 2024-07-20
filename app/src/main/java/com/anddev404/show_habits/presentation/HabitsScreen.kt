b36a59b91980899b5af411e367ac52446d0a5f2f
anddev404 <marcinek07@gmail.com>
2024-07-20 17:47:03 +0200

    Implement habit display

--- app/src/main/java/com/anddev404/show_habits/presentation/HabitsScreen.kt
+++ app/src/main/java/com/anddev404/show_habits/presentation/HabitsScreen.kt
@@ -1,31 +1,64 @@
 package com.anddev404.show_habits.presentation
 
-import androidx.compose.foundation.layout.Column
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.anddev404.navigation.Screen
 import com.anddev404.show_habits.components.main_top_bar.MainTopBar
+import com.anddev404.show_habits.presentation.habits_view.HabitsEmptyView
+import com.anddev404.show_habits.presentation.habits_view.HabitsView
+import com.anddev404.show_habits.presentation.habits_view.events.HabitsViewRowEvent
+import com.anddev404.show_habits.presentation.habits_view.states.HabitsViewItemState
 import com.anddev404.show_habits.presentation.main_top_bar.MainTopBarEvents
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun HabitsScreen(navController: NavController) {
-    Scaffold(modifier = Modifier.fillMaxSize(),
-        topBar = {
-            MainTopBar() {
-                when (it) {
-                    is MainTopBarEvents.OnAddHabitClick -> {
-                        navController.navigate(Screen.CreateHabitScreen(it.event.contentId))
-                    }
+fun HabitsScreen(navController: NavController, viewModel: HabitsViewModel = hiltViewModel()) {
+    Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+        MainTopBar() {
+            when (it) {
+                is MainTopBarEvents.OnAddHabitClick -> {
+                    navController.navigate(Screen.CreateHabitScreen(it.event.contentId))
                 }
             }
-        }) { innerPadding ->
-        Column {
-            Text(modifier = Modifier.padding(innerPadding), text = "Habits ... ")
+        }
+    }) { innerPadding ->
+
+        val habitsState by viewModel.habitsView.collectAsState()
+
+        if (habitsState.isEmpty()) {
+            HabitsEmptyView(Modifier.padding(innerPadding))
+            return@Scaffold
+        }
+
+        HabitsView(
+            Modifier.padding(innerPadding),
+            headerState = viewModel.header,
+            habitsStates = habitsState
+        ) { event ->
+            when (event) {
+                is HabitsViewRowEvent.OnItemClick -> {
+
+                    when (event.item) {
+                        is HabitsViewItemState.Value -> TODO()
+
+                        is HabitsViewItemState.YesOrNo -> {
+                            viewModel.updateItem(event.item.copy(isChecked = !event.item.isChecked))
+                        }
+                    }
+                }
+
+                is HabitsViewRowEvent.OnRowClick -> TODO()
+                is HabitsViewRowEvent.OnRowLongClick -> TODO()
+            }
         }
     }
 }
--- app/src/main/java/com/anddev404/show_habits/presentation/HabitsViewModel.kt
+++ app/src/main/java/com/anddev404/show_habits/presentation/HabitsViewModel.kt
@@ -1,6 +1,158 @@
 package com.anddev404.show_habits.presentation
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.anddev404.core.data.local.entity.HabitEntity
+import com.anddev404.core.data.local.entity.ItemEntity
+import com.anddev404.core.domain.repository.HabitRepository
+import com.anddev404.show_habits.domain.DateGenerator.getCustomDatesFromToday
+import com.anddev404.show_habits.presentation.habits_view.states.HabitsViewHeaderItemState
+import com.anddev404.show_habits.presentation.habits_view.states.HabitsViewItemState
+import com.anddev404.show_habits.presentation.habits_view.states.HabitsViewRowState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class HabitsViewModel : ViewModel() {
+@RequiresApi(Build.VERSION_CODES.O)
+@HiltViewModel
+class HabitsViewModel @Inject constructor(private val repository: HabitRepository) : ViewModel() {
+
+    private val _habitState = MutableStateFlow(listOf<HabitEntity>())
+    private val _itemState = MutableStateFlow(listOf<ItemEntity>())
+
+    private val _habitsView = MutableStateFlow(listOf<HabitsViewRowState>())
+    val habitsView = _habitsView.asStateFlow()
+    var header = listOf<HabitsViewHeaderItemState>()
+
+    init {
+        header = getCustomDatesFromToday(30).map {
+            HabitsViewHeaderItemState(
+                it.dayOfWeek, it.day, it.month, it.year
+            )
+        }
+
+        getHabits()
+        getItems()
+
+        _habitState.combine(_itemState) { habits, allItems ->
+            _habitsView.value = habits.map { habit ->
+                val items = arrayListOf<HabitsViewItemState>()
+                val filteredItems = allItems.filter { it.habitId == habit.id }
+
+                header.forEach { header ->
+
+                    val item = when (habit.habitType) {
+                        1 -> HabitsViewItemState.YesOrNo(
+                            0,
+                            habit.id,
+                            false,
+                            Color(habit.color),
+                            header.getTimeInMilliseconds()
+                        )
+
+                        2 -> HabitsViewItemState.Value(
+                            0,
+                            habit.id,
+                            "kg",
+                            10.0,
+                            Color(habit.color),
+                            header.getTimeInMilliseconds()
+                        )
+
+                        else -> {
+                            HabitsViewItemState.YesOrNo(
+                                0,
+                                habit.id,
+                                false,
+                                Color(habit.color),
+                                header.getTimeInMilliseconds()
+                            )
+                        }
+                    }
+                    items.add(item)
+                }
+
+                for (i in items) {
+                    for (f in filteredItems) {
+
+                        when (i) {
+                            is HabitsViewItemState.Value -> {
+                                if (i.date == f.date) {
+                                    i.date = f.date
+                                    i.id = f.id
+                                    i.value = f.value
+                                    i.unit = habit.unit
+                                }
+                            }
+
+                            is HabitsViewItemState.YesOrNo -> {
+                                if (i.date == f.date) {
+                                    i.date = f.date
+                                    i.id = f.id
+                                    i.isChecked = f.isChecked
+                                }
+                            }
+                        }
+                    }
+                }
+
+                HabitsViewRowState(
+                    name = habit.name, color = Color(habit.color), itemList = items
+                )
+            }
+        }.launchIn(viewModelScope)
+    }
+
+    private fun getHabits() {
+
+        viewModelScope.launch {
+            launch {
+                repository.getAllHabits()
+                    .collect {
+                        _habitState.value = it
+                    }
+            }
+        }
+    }
+
+    fun updateItem(item: HabitsViewItemState) {
+
+        val entity = when (item) {
+            is HabitsViewItemState.Value -> ItemEntity(
+                item.date,
+                false,
+                item.value,
+                item.habitId,
+                item.id
+            )
+
+            is HabitsViewItemState.YesOrNo -> ItemEntity(
+                item.date,
+                item.isChecked,
+                0.0,
+                item.habitId,
+                item.id
+            )
+        }
+        viewModelScope.launch {
+            repository.insertItem(entity)
+        }
+    }
+
+    private fun getItems() {
+        viewModelScope.launch {
+            if (header.isEmpty()) return@launch
+
+            repository.getItemsAfterDate(header.last().getTimeInMilliseconds()).collect() {
+                _itemState.value = _itemState.value.toMutableList().apply { addAll(it) }
+            }
+        }
+    }
 }
