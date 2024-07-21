0dd81e774b0dad86af5c5943c3498529de7b5b90
anddev404 <marcinek07@gmail.com>
2024-07-21 19:42:52 +0200

    Add fun provideDayOfWeek to AppModule

--- app/src/main/java/com/anddev404/di/AppModule.kt
+++ app/src/main/java/com/anddev404/di/AppModule.kt
@@ -1,10 +1,13 @@
 package com.anddev404.di
 
 import android.app.Application
+import android.content.Context
 import androidx.room.Room
 import com.anddev404.core.data.local.HabitDatabase
 import com.anddev404.core.data.repository.HabitRepositoryImpl
 import com.anddev404.core.domain.repository.HabitRepository
+import com.anddev404.show_habits.data.DayOfWeekImpl
+import com.anddev404.show_habits.domain.model.DayOfWeek
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -29,5 +32,11 @@ object AppModule {
     @Singleton
     fun provideHabitRepository(database: HabitDatabase): HabitRepository {
         return HabitRepositoryImpl(database)
+    }
+
+    @Provides
+    @Singleton
+    fun provideDayOfWeek(app: Application): DayOfWeek {
+        return DayOfWeekImpl(app)
     }
 }
